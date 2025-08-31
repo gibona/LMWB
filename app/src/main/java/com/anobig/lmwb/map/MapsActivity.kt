85bb9b57dee5cab7c0f090ad04c539d8eed9d824
Gibona <gibona@gmail.com>
2025-08-31 13:59:12 +0300

    Added current use location + permissions

--- app/src/main/java/com/anobig/lmwb/map/MapsActivity.kt
+++ app/src/main/java/com/anobig/lmwb/map/MapsActivity.kt
@@ -1,7 +1,14 @@
 package com.anobig.lmwb.map
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresPermission
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.anobig.lmwb.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,9 +18,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.anobig.lmwb.databinding.ActivityMapsBinding
 import com.anobig.lmwb.map.db.DbInterface
+import com.anobig.lmwb.utility.App
+import com.google.android.gms.location.LocationServices
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val TAG: String = "MapsActivity"
+    private val DEFAULT_ZOOM = 16f
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: Int = 1001
+    private var locationPermissionGranted: Boolean = false
     private lateinit var googleMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
@@ -27,6 +40,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        getLocationPermission()
     }
 
     /**
@@ -38,15 +53,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         this@MapsActivity.googleMap = googleMap
         // move the camera to Sofia
         val sofia = LatLng(42.65897, 23.31781)
 
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(16.0f))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(DEFAULT_ZOOM))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sofia))
-
-
 
         googleMap.setOnMapLongClickListener { latLng ->
             DbInterface.addPinDialogShow(
@@ -60,5 +74,82 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         DbInterface.observePinChanges(this, this.googleMap)
+
+        updateLocationUI()
+
     }
+
+    fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(
+                App.get(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+            updateLocationUI()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        locationPermissionGranted = false
+        if (requestCode
+            == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.isNotEmpty()
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        updateLocationUI()
+    }
+    
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    fun updateLocationUI() {
+        try {
+            if (locationPermissionGranted) {
+                googleMap.isMyLocationEnabled = true
+                googleMap.uiSettings.isMyLocationButtonEnabled = true
+                val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(task.result!!.latitude,
+                                    task.result!!.longitude), DEFAULT_ZOOM))
+                    }
+                    else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        googleMap.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+
+            } else {
+                googleMap.isMyLocationEnabled = false
+                googleMap.uiSettings.isMyLocationButtonEnabled = false
+            }
+        } catch (e: Exception)  {
+            e.printStackTrace()
+        }
+}
 }
